fd9f4188c59f3740d977ec8a99c52b4b1eea659f
Alex Chan <alex@alexwlchan.net>
2017-07-22 15:38:34 +0100

    Design the CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![deny(warnings)]
+
+extern crate docopt;
+#[macro_use]
+extern crate serde_derive;
+
+mod cli;
+
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+  let args = cli::parse_args(NAME);
+
+  if args.flag_version {
+    println!("{} v{}", NAME, VERSION);
+  }
+
+  println!("{:?}", args);
 }
