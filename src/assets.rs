3cd94cffef7382451520d0bfeaeee3d2b575c8bd
Alex Chan <alex@alexwlchan.net>
2017-07-22 22:11:48 +0100

    Get the cookie stuff working for logins

--- src/assets.rs
+++ src/assets.rs
@@ -1,17 +1,16 @@
 use std::collections::HashMap;
+use std::io::Read;
 use std::process;
 
-use hyper::header::Location;
+use hyper::header::Header;
+use reqwest::header::{Cookie, Location, SetCookie};
 use reqwest::{Client, RedirectPolicy};
 
 
-/// Return a map from URLs to Pinboard cache IDs.
+/// Log in to Pinboard and get a login cookie that can be used on subsequent
+/// requests.
 ///
-///  - `username`: Pinboard username
-///  - `password`: Pinboard password
-///
-pub fn get_cache_ids(username: String, password: String) -> HashMap<String, String> {
-
+fn get_login_cookie(username: &str, password: &str) -> Cookie {
     // Start by logging in to Pinboard, so we have the appropriate cookies.
     // Because Pinboard sends us into a weird redirect loop, we have to
     // tell reqwest not to follow redirects, just check the redirect worked.
@@ -23,12 +22,11 @@ pub fn get_cache_ids(username: String, password: String) -> HashMap<String, Stri
     println!("foo");
     let resp = client.post("https://pinboard.in/auth/")
         .unwrap()
-        .form(&[("username", username), ("password", password)])
+        .form(&[("username", &username), ("password", &password)])
         .unwrap()
         .send();
-    let login_successful = resp.ok()
-        .unwrap()
-        .headers()
+    let headers = resp.ok().unwrap().headers().to_owned();
+    let login_successful = headers
         .get::<Location>()
         .unwrap() != &Location::new("?error=wrong+password");
     if !login_successful {
@@ -36,7 +34,47 @@ pub fn get_cache_ids(username: String, password: String) -> HashMap<String, Stri
         process::exit(1);
     }
 
-    println!("{:?}", login_successful);
+    // Cookie-handling code.  This should probably be handled by reqwest,
+    // but I've given up trying to get it working, and I'm just doing it
+    // by hand instead.  Pinboard only sets four interesting cookie values.
+    let set_cookie = headers
+        .get::<SetCookie>()
+        .unwrap();
+    let mut cookie = Cookie::new();
+    for c in set_cookie.iter() {
+        let parsed = Cookie::parse_header(&c.as_bytes().into()).unwrap();
+        for key in vec!["groznaz", "auth", "secauth", "login"].iter() {
+            match parsed.get(key) {
+                Some(val) => cookie.set(key.to_owned(), val.to_owned()),
+                None => {},
+            };
+        }
+    }
+
+    cookie
+}
+
+
+/// Return a map from URLs to Pinboard cache IDs.
+///
+///  - `username`: Pinboard username
+///  - `password`: Pinboard password
+///
+pub fn get_cache_ids(username: String, password: String) -> HashMap<String, String> {
+
+    let client = Client::new().unwrap();
+    let cookie = get_login_cookie(&username, &password);
+
+    // Now fetch a blob of HTML for the first page.
+    let url = format!("https://pinboard.in/u:{}/", username);
+    let resp = client.get(&url)
+        .unwrap()
+        .header(cookie)
+        .send();
+    let mut content = String::new();
+    let _ = resp.ok().unwrap().read_to_string(&mut content);
+
+    // println!("{:?}", content);
 
     HashMap::new()
 }
